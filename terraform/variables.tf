29e9126b4eb1794001def059acde60f8158b81c1
devi <devimeena.iiit@gmail.com>
2023-10-14 20:46:08 +0530

    catalogue deployment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,8 @@ variable "env" {
     default = "dev"
 }
 
+variable "app_version" {
+    # this is just to test variable is flowing from terraform to shell and then to ansible
+   default =  "100.100.100"
+}
+
